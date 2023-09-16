8f8b56f61f028bea8602fb3f38121b38db09b187
JamieC99 <jamie.h.coleman@gmail.com>
2023-09-16 17:11:39 +1200

    Update Window.java

--- src/main/Window.java
+++ src/main/Window.java
@@ -4,6 +4,8 @@ import input.*;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -15,13 +17,17 @@ public class Window extends JPanel
 	
 	// Create frame
 	JFrame frame = new JFrame("Adventure RPG");
+	
+	// Default window size
 	private final int WIDTH = 1552, HEIGHT = 999;
+	
+	private float windowScale;
 	
 	public Window()
 	{
 		// Setup window
 		frame.setSize(WIDTH, HEIGHT);
-		frame.setResizable(false);
+		frame.setResizable(true);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
@@ -38,10 +44,21 @@ public class Window extends JPanel
 	{
 		super.paintComponent(g);
 		
-		// Fill black background
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, WIDTH, HEIGHT);
+		Graphics2D g2d = (Graphics2D) g;
 		
+		// Apply scaling to the window
+		AffineTransform transform = new AffineTransform();
+		
+		// Calculate the window scale
+		windowScale = 1.0f * frame.getWidth() / WIDTH;
+		
+		//frame.setSize(frame.getWidth(), (int) (frame.getWidth() * windowScale));
+		
+		// Apply scale
+		transform.scale(windowScale, windowScale);
+		g2d.setTransform(transform);
+		
+		// Draw background
 		g.drawImage(new ImageIcon("resources/sprites/environment/background.png").getImage(), 0, 0, 1536, 960, null);
 		
 		// Draw game objects
