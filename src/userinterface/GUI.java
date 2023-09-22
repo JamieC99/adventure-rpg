b2d4ed5b5fecc94e8b128bea81935ccb0aa0f170
JamieC99 <jamie.h.coleman@gmail.com>
2023-09-23 11:17:03 +1200

    Update GUI.java

--- src/userinterface/GUI.java
+++ src/userinterface/GUI.java
@@ -12,52 +12,56 @@ public class GUI
 {
 	public void paintComponent(Graphics g)
 	{
-		// Set font
-		Font originalFont = g.getFont();
-		Font playerNameFont = originalFont.deriveFont(Font.BOLD, 14);
-		g.setFont(playerNameFont);
-		
-		// Display player names
-		for (int i = 0; i < Handler.getObjectList().size(); i++)
+		if (!Handler.modifyingObjectList)
 		{
-			GameObject object = Handler.getObjectList().get(i);
+			// Set font
+			Font originalFont = g.getFont();
+			Font playerNameFont = originalFont.deriveFont(Font.BOLD, 14);
+			g.setFont(playerNameFont);
 			
-			if (object instanceof PlayerCharacter)
+			// Display player names
+			for (int i = 0; i < Handler.getObjectList().size(); i++)
 			{
-				PlayerCharacter playerObject = (PlayerCharacter) object;
+				GameObject object = Handler.getObjectList().get(i);
 				
-				if (playerObject.getPlayerNumber() == 1)
+				if (object instanceof PlayerCharacter)
 				{
-					// Draw background
-					g.setColor(Color.BLACK);
-					g.fillRect(playerObject.getX()-8, playerObject.getY()-20, 64, 16); // Draw name box
+					PlayerCharacter playerObject = (PlayerCharacter) object;
 					
-					// Draw name
-					g.setColor(Color.CYAN);
-					g.drawString("Player 1", playerObject.getX()-2, playerObject.getY()-8);
-					
-					// Draw health bar
-					g.setColor(Color.RED);
-					g.fillRect(playerObject.getX()-8, playerObject.getY()-4, playerObject.getHealthPoints() / 100 * 64, 4);
-				}
-				else if (playerObject.getPlayerNumber() == 2)
-				{
-					// Draw background
-					g.setColor(Color.BLACK);
-					g.fillRect(playerObject.getX()-8, playerObject.getY()-20, 64, 16); // Draw name box
-					
-					// Draw name
-					g.setColor(Color.ORANGE);
-					g.drawString("Player 2", playerObject.getX()-2, playerObject.getY()-8);
-					
-					// Draw health bar
-					g.setColor(Color.RED);
-					g.fillRect(playerObject.getX()-8, playerObject.getY()-4, playerObject.getHealthPoints() / 100 * 64, 4);
+					if (playerObject.getPlayerNumber() == 1)
+					{
+						// Draw background
+						g.setColor(Color.BLACK);
+						g.fillRect(playerObject.getX()-8, playerObject.getY()-20, 64, 16); // Draw name box
+						
+						// Draw name
+						g.setColor(Color.CYAN);
+						g.drawString("Player 1", playerObject.getX()-2, playerObject.getY()-8);
+						
+						// Draw health bar
+						g.setColor(Color.RED);
+						g.fillRect(playerObject.getX()-8, playerObject.getY()-4, playerObject.getHealthPoints() / 100 * 64, 4);
+					}
+					else if (playerObject.getPlayerNumber() == 2)
+					{
+						// Draw background
+						g.setColor(Color.BLACK);
+						g.fillRect(playerObject.getX()-8, playerObject.getY()-20, 64, 16); // Draw name box
+						
+						// Draw name
+						g.setColor(Color.ORANGE);
+						g.drawString("Player 2", playerObject.getX()-2, playerObject.getY()-8);
+						
+						// Draw health bar
+						g.setColor(Color.RED);
+						g.fillRect(playerObject.getX()-8, playerObject.getY()-4, playerObject.getHealthPoints() / 100 * 64, 4);
+					}
 				}
 			}
+		
+			g.setFont(originalFont);
 		}
 		
-		g.setFont(originalFont);
 	}
 	
 	public void tick()
